4d5c2e07c66c38b8e313775341f024cd76fc8b93
Helen Read <helen.eliz.read@gmail.com>
2023-03-27 18:22:04 -0400

    reran bender for v400 and v410

--- shell-nu-sweep/_run_nu_sweep.py
+++ shell-nu-sweep/_run_nu_sweep.py
@@ -6,7 +6,7 @@ from buck_cylinder_obj import *
 from geo_prop import *
 from time import time
 
-idx_try = 400
+idx_try = 410
 bdamp = 0.0001
 # proj_name = '2fold-test_nu-'
 proj_name = 'bender-test_nu-'
@@ -39,7 +39,7 @@ for i, nu in enumerate(nu_try):
     # test.mesh_order = 'quadratic'
     test.nu_shell = nu
     jname_lin = test.run_linear_model()
-    jname_multi = test.make_nonlin_multi_buckle(bdamp, max_temp_mult = 0.35, num_steps = 30)
+    jname_multi = test.make_nonlin_multi_buckle(bdamp, max_temp_mult = 0.35, num_steps = 30, eig_idx = 2)
     # jname_riks = test.make_riks_model(bdamp, temp_mult = 0.3)
 
     run_inp(jname_multi)
@@ -47,7 +47,7 @@ for i, nu in enumerate(nu_try):
 
     test.post_process_multi_buckle()
 
-    delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log'])
+    delete_extra_files(jname_lin, ['.fil', '.sta', '.log'])
     # delete_extra_files(jname_riks)
     delete_extra_files(jname_multi)
 
@@ -62,7 +62,7 @@ for i, nu in enumerate(nu_try):
 #250s series: multi up to 0.35 and 30 steps w/ tri elems
 #[didn't work] 260s series: multi up to 0.35 and 30 steps w/ 2nd order quad elems
 #300s series: 2 folds multi up to 0.35 and 30 steps
-#400s series: bender up to 0.35 and 30 steps
+#400s series: bender up to 0.35 and 30 steps: they all failed early due to 1st eigenmode being the non bending one
 
 
 
