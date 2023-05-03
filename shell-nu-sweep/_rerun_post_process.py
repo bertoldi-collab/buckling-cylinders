d5bc6213c8a1152fe0638c549eddd65558585a9b
Helen Read <helen.eliz.read@gmail.com>
2023-05-03 17:05:11 -0400

    ran multi buckling 2 folds

--- shell-nu-sweep/_rerun_post_process.py
+++ shell-nu-sweep/_rerun_post_process.py
@@ -5,14 +5,14 @@ sys.path.append('../')
 from buck_cylinder_obj import *
 from geo_prop import *
 
-idx_try = 100
-idx_last = 261
+idx_try = 300
+idx_last = 350
 
 
 
 for i in range(idx_try, idx_last):
-    odb_name = '4fold-test_nu-' + str(i) + '_multi_buckling.odb'
-    proj_name = '4fold-test_nu-' + str(i)
+    odb_name = '2fold-test_nu-' + str(i) + '_multi_buckling.odb'
+    proj_name = '2fold-test_nu-' + str(i)
     if os.path.exists(odb_name):
-        instance = full_shell(proj_name, simpProps = geo_prop_four)
+        instance = full_shell(proj_name, simpProps = geo_prop_two)
         instance.post_process_multi_buckle()
