de9e896a14dab87a8cd2b181b9556e39b8836883
Helen Read <helen.eliz.read@gmail.com>
2023-05-15 17:57:11 -0400

    ran linear buckling modes for figure

--- sims-figures/_run_sims_fig.py
+++ sims-figures/_run_sims_fig.py
@@ -5,9 +5,9 @@ sys.path.append('../')
 from buck_cylinder_obj import *
 from geo_prop import *
 
-sim_type = '4folds'
-geo_props_use = geo_prop_four
-version = 104
+sim_type = '2folds'
+geo_props_use = geo_prop_two
+version = 107
 
 final_temp_mult = 0.9
 bdamp = 0.001
@@ -17,14 +17,14 @@ proj_name = 'sim-long-' + sim_type + '-' + str(version)
 
 test = full_shell(project = proj_name, simpProps = geo_props_use, imperfection = 0.001)
 
-test.h_element = elem_size_mult * test.h_element
+# test.h_element = elem_size_mult * test.h_element
 jname_lin = test.run_linear_model()
-jname_nonlin = test.make_nonlin_model(bdamp, temp_set = final_temp_mult*-0.332)
-run_inp(jname_nonlin,6)
+# jname_nonlin = test.make_nonlin_model(bdamp, temp_set = final_temp_mult*-0.332)
+# run_inp(jname_nonlin,6)
 
-test.post_process_pv()
-delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log'])
-delete_extra_files(jname_nonlin)
+# test.post_process_pv()
+# delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log'])
+# delete_extra_files(jname_nonlin)
 
 #FOUR FOLDS
 #v100: ran normal, failed at 0.777: looks like we got local hourglassing that caused the sample to bend
@@ -32,3 +32,8 @@ delete_extra_files(jname_nonlin)
 #v102: trying 0.5*mesh size: showed more oscillation post 2nd instab
 #v103: bumping bdamp from 0.0001 to 0.0005
 #v104: bumping bdamp from 0.0005 to 0.001
+
+#LINEAR BUCKLING ONLY
+#v105: just linear buckling 4folds
+#v106: just linear buckling 3folds
+#v107: just linear buckling 2folds
