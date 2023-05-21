ea13286b424106aeb16adef689d3f9acb2cc4587
Helen Read <helen.eliz.read@gmail.com>
2023-05-21 12:54:20 -0400

    ran num folds lin buckling test for 3folds geometries

--- three-folds-geo-testing/_run_three_sweep.py
+++ three-folds-geo-testing/_run_three_sweep.py
@@ -5,29 +5,44 @@ sys.path.append('../')
 from buck_cylinder_obj import *
 from geo_prop import *
 
-idx_try = 100
+idx_try = 500
 bdamp = 0.0001
 proj_name = '3fold-geo-test-'
 
+#original lin buckle sweep
+# t_try = np.linspace(0.5,0.6,20)
+# H_try = np.linspace(30,35,10)
+# R_try = [8.8, 10]
 
-t_try = np.linspace(0.5,0.6,20)
-H_try = np.linspace(30,35,10)
-R_try = [8.8, 10]
+#reduced multi buckle sweep
+t_try = np.linspace(0.5,0.6,5)
+H_try = np.linspace(30,35,5)
+R_try = [8.8]
 
-for i in range(len(E_try)):
-    E_cur = E_try[i]
-    for j in range(len(t_try)):
-        # printAB(idx_try)
-        t_cur = t_try[j]
-        geo_prop_cur = geoProps(10, 27, 5, t_cur, E_cur) #all else is taken from geo_prop_three
+# num_folds_all = np.zeros((len(R_try), len(H_try), len(t_try)))
 
-        test = full_shell(project = proj_name+str(idx_try), simpProps = geo_prop_cur)
-        jname_lin = test.run_linear_model()
+for i in range(len(R_try)):
+    R_cur = R_try[i]
+    for j in range(len(H_try)):
+        H_cur = H_try[j]
+        for k in range(len(t_try)):
+            t_cur = t_try[k]
+            geo_prop_cur = geoProps(R_cur, H_cur, 5, t_cur, 1.4) #all else is taken from geo_prop_three
 
-        num_folds = test.post_process_num_folds()
+            test = full_shell(project = proj_name+str(idx_try), simpProps = geo_prop_cur, imperfection = 0.05)
+            jname_lin = test.run_linear_model()
+            jname_multi = test.make_nonlin_multi_buckle(bdamp, max_temp_mult = 0.45, num_steps = 50)
 
-        delete_extra_files(jname_lin, ['.fil', '.sta', '.log'])
+            run_inp(jname_multi)
+            # num_folds_all[i,j,k] = num_folds
 
-        idx_try += 1
+            delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log', '.dat', '.msg'])
+            delete_extra_files(jname_multi, ['.log', '.dat', '.msg'])
+
+            idx_try += 1
+# printAB(num_folds_all)
+# np.savetxt('_3folds_num_folds_mode_1.txt', np.reshape(num_folds_all, (len(H_try), len(R_try)*len(t_try))))
+
+#v100-v499: lin sweep of parameter space
 
 
