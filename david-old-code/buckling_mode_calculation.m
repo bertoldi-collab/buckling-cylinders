bb06ba6fab9ea363c040535c4b40d217efa3ab35
Helen Read <helen.eliz.read@gmail.com>
2023-06-01 11:35:44 -0400

    figure making

--- david-old-code/buckling_mode_calculation.m
+++ david-old-code/buckling_mode_calculation.m
@@ -25,7 +25,9 @@ for p = 1:length(alpha)
     end
 end
 %M = flipud(N)
-figure
+
+%%
+figure(1)
 %h = heatmap(beta,alpha,N,'CellLabelColor','none')
 %contourf(beta,alpha,N,'--');
 h = surf(beta,alpha,N,'FaceAlpha',1);
@@ -39,19 +41,37 @@ view(0,90)
 xlabel('t/H','FontSize',16);
 ylabel('R/H','FontSize',16);
 
-%find_n_alt(0.2, 0.025)
-n_calc(0.025, 0.2)
+figure(2)
+%h = heatmap(beta,alpha,N,'CellLabelColor','none')
+%contourf(beta,alpha,N,'--');
+h = surf(beta,alpha,p_cr,'FaceAlpha',1);
+colorbar
+caxis([0 0.005])
+h.EdgeColor = 'none';
+set(gca,'FontSize',16)
+pbaspect([1 1 1])
+%grid off
+shading interp;
+colormap(parula(8));
+view(0,90)
+xlabel('t/H','FontSize',16);
+ylabel('R/H','FontSize',16);
 
-function n_find = n_calc(aa,bb)
+
+%%
+
+%find_n_alt(0.2, 0.025)
+[n_sp, p_sp] = n_calc(0.1, 0.05)
+
+function [n_find, p_find] = n_calc(aa,bb)
+%aa: R/H, bb: t/H
 n=[2,3,4,5,6,7,8]; % initial guess of fold numbers
 nu = 0.5;
 p_n = zeros(length(n),1);
 for i = 1:length(n)
     p_n(i) = ((pi*aa)^2+n(i)^2)^2*(bb/aa)^3/(12*(1-nu^2)*n(i)^2)+...
         (pi*aa)^4*(bb/aa)/(n(i)^2*((pi*aa)^2+n(i)^2)^2);
-        %Z(i)=(1/aa*1/bb)*sqrt(1-nu^2);
-    %p_n(i) = (1+n(i)^2)^2/n(i)^2+(12/pi^4)*Z(i)^2/(n(i)^2*(1+n(i)^2)^2);
 end
-[~,I] = min(p_n);
+[p_find,I] = min(p_n);
 n_find = n(I);
 end
--- sims-figures/_run_sims_fig.py
+++ sims-figures/_run_sims_fig.py
@@ -7,7 +7,7 @@ from geo_prop import *
 
 sim_type = '2folds'
 geo_props_use = geo_prop_two
-version = 107
+version = 111
 
 final_temp_mult = 0.75
 bdamp = 0.001
@@ -15,20 +15,25 @@ elem_size_mult = 0.5
 
 proj_name = 'sim-long-' + sim_type + '-' + str(version)
 
-test = full_shell(project = proj_name, simpProps = geo_props_use, imperfection = 0.001)
+test = full_shell(project = proj_name, simpProps = geo_props_use, imperfection = 0.05)
+# test.static_stable = False
 
 # test.h_element = elem_size_mult * test.h_element
-# jname_lin = test.run_linear_model()
+jname_lin = test.run_linear_model()
+# jname_multi = test.make_nonlin_multi_buckle(bdamp, max_temp_mult = 0.55, num_steps = 50)
 # jname_nonlin = test.make_nonlin_model(bdamp, temp_set = final_temp_mult*-0.332)
 # run_inp(jname_nonlin,6)
+# run_inp(jname_multi)
+# test.post_process_multi_buckle()
 
 # test.post_process_pv()
 # test.post_process_contraction_twist()
-# delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log'])
+delete_extra_files(jname_lin, ['.fil', '.sta', '.odb', '.log'])
+# delete_extra_files(jname_multi)
 # delete_extra_files(jname_nonlin)
 
-num_folds = test.post_process_num_folds()
-printAB(num_folds)
+# num_folds = test.post_process_num_folds()
+# printAB(num_folds)
 
 #FOUR FOLDS
 #v100: ran normal, failed at 0.777: looks like we got local hourglassing that caused the sample to bend
@@ -42,7 +47,12 @@ printAB(num_folds)
 #v106: just linear buckling 3folds
 #v107: just linear buckling 2folds
 
-#long 3 and 4
+#long 2/3/4
 #v108: 3folds up to 0.75
 #v109: 4folds up to 0.75
-#v110: 2folds up to 0.75
+#v110: 2folds up to 0.75 + multi buckling (up to 0.55)
+
+#long testing 2
+#v111: 2folds mutli up to 0.55 w/ static stable
+
+
