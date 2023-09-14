857291f803973de24c43c8dab202abbbaa334ac7
Helen Read <helen.eliz.read@gmail.com>
2023-09-14 11:53:18 -0400

    sc of modes + nb changes for fig2

--- bender-sims/_run_sims_bender.py
+++ bender-sims/_run_sims_bender.py
@@ -6,29 +6,53 @@ from cylinder_obj import *
 from geo_prop import *
 
 
-idx_try = 200
+def damping_sweep(idx_try):
+    num_samp = 5
+    damping_sweep = 2*np.logspace(-8, -4, num_samp)
 
-num_samp = 5
-damping_sweep = 2*np.logspace(-8, -4, num_samp)
+    for i, stab_factor in enumerate(damping_sweep):
+        proj_name = 'bender-static-stable-v' + str(idx_try + i)
+        test = full_shell(project = proj_name, fullProps = geo_prop_bend, imperfection = 0.1)
 
-for i, stab_factor in enumerate(damping_sweep):
-    proj_name = 'bender-static-stable-v' + str(idx_try + i)
-    test = full_shell(project = proj_name, fullProps = geo_prop_bend, imperfection = 0.05)
+        test.stabilization_factor = stab_factor
 
-    test.stabilization_factor = stab_factor
+        jname_lin = test.run_linear_model()
+        jname_multi = test.make_nonlin_multi_buckle(max_temp_mult = 0.5, num_steps = 100, eig_idx = 2)
+
+        run_inp(jname_multi)
+
+        test.post_process_multi_pv()
+        test.post_process_multi_buckle()
+
+        delete_extra_files(jname_lin, ['.fil', '.sta', '.log', '.dat', '.msg'])
+        delete_extra_files(jname_multi)
+
+def dyn_imp(idx_try):
+    proj_name = 'bender-dyn-imp-v' + str(idx_try)
+    test = full_shell(project = proj_name, fullProps = geo_prop_bend, imperfection = 0.001)
+
 
     jname_lin = test.run_linear_model()
-    jname_multi = test.make_nonlin_multi_buckle(max_temp_mult = 0.5, num_steps = 100)
+    jname_nonlin = test.make_nonlin_model(temp_mult = 0.5)
 
-    run_inp(jname_multi)
+    run_inp(jname_nonlin)
 
     test.post_process_multi_pv()
     test.post_process_multi_buckle()
 
     delete_extra_files(jname_lin, ['.fil', '.sta', '.log', '.dat', '.msg'])
-    delete_extra_files(jname_multi)
+    delete_extra_files(jname_nonlin)
+
+def main():
+    idx_try = 140
+    dyn_imp(idx_try)
+
+main()
+
+#v140: dyn imp bender
 
 #v200s: bender multi, damping_sweep = 2*np.logspace(-8, -4, num_samp), steps = 100, max_temp_mult = 0.5, imperfection = 0.05
+#v210s: bender multi, damping_sweep = 2*np.logspace(-8, -4, num_samp), steps = 100, max_temp_mult = 0.5, imperfection = 0.1, eig_idx = 2
 
 # idx_try = 100
 # bdamp = 0.0001
